e4eaf480808060c17cebc48109f375ec5145417f
felipeknt <felipe_cardoso_007@hotmail.com>
2021-12-28 19:42:04 -0400

    Criacao da classe Elevador e adicionado projeto WEB

---
+++ ProjetoFinalJornadaOcean/Model/Elevador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinalJornadaOcean.Model
+{
+    internal class Elevador
+    {
+        public void Inicializar() { 
+
+            //Definir quantidade de andares.
+
+            //Definir a capacidade de pessoas.
+        }
+
+        public void Entrar() { 
+        }
+
+        public void Sair() { 
+        }
+
+        public void Subir() { 
+        }
+
+        public void Descer() { 
+        }
+    }
+}
