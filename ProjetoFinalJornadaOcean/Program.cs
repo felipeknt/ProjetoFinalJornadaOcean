bca30eb5a1a426db80efdad6ce8f76384cf8f48c
felipeknt <felipe_cardoso_007@hotmail.com>
2021-12-30 22:32:07 -0400

    Adicionado view

--- ProjetoFinalJornadaOcean/Program.cs
+++ ProjetoFinalJornadaOcean/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetoFinalJornadaOcean.Views;
 
 namespace ProjetoFinalJornadaOcean
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            ConsoleViewElevador viewElevador = new ConsoleViewElevador();
+            viewElevador.UsarElevador();
         }
     }
 }
---
+++ ProjetoFinalJornadaOcean/Views/ConsoleViewElevador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoFinalJornadaOcean.Model;
+
+namespace ProjetoFinalJornadaOcean.Views
+{
+    /// <summary>
+    /// Classe de interação do elevador
+    /// </summary>
+    internal class ConsoleViewElevador
+    {
+        Elevador elevador = new Elevador();
+
+        public void UsarElevador() {
+            ConfiguracaoConsole();
+            Console.WriteLine("Projeto - ELEVADOR");
+            Console.WriteLine("BEM VINDO");
+            //Console.
+            Console.ReadKey();
+        }
+        public static void LimparConsole(){
+            Console.Clear();
+        }
+
+        private void ConfiguracaoConsole() {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
