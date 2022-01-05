acf74537913171deded400dfc4104901f18ba0c1
felipeknt <felipe_cardoso_007@hotmail.com>
2022-01-04 23:14:08 -0400

    Interação com o usuário

--- ProjetoFinalJornadaOcean/Views/ConsoleViewElevador.cs
+++ ProjetoFinalJornadaOcean/Views/ConsoleViewElevador.cs
@@ -17,31 +17,61 @@ namespace ProjetoFinalJornadaOcean.Views
         //Nova instancia da classe Elevador
         Elevador e = new Elevador();
 
-        public void UsarElevador() {
+        public void UsarElevador()
+        {
             ConfiguracaoConsole();
             Painel();
             Console.ReadKey();
         }
-        public static void LimparConsole(){
+        public static void LimparConsole()
+        {
             Console.Clear();
         }
 
-        private void ConfiguracaoConsole() {
+        private void ConfiguracaoConsole()
+        {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Title = "Projeto elevador";
         }
 
-        private void Painel() {
+        private void Painel()
+        {
             Console.WriteLine("╔══════════════════════╗");
             Console.WriteLine($"║ Status: {status}   ║");
             Console.WriteLine($"║ Andar: {e.AndarAtual.ToString("00")}            ║");
             Console.WriteLine($"║ Pessoas: {e.Pessoas.ToString("00")}          ║");
             //Console.WriteLine("║                      ║");
-           // Console.WriteLine("║                      ║");
-           // Console.WriteLine("║                      ║");
+            // Console.WriteLine("║                      ║");
+            // Console.WriteLine("║                      ║");
             //Console.WriteLine("║                      ║");
             Console.WriteLine("╚══════════════════════╝");
 
+        }
+
+        private void Entrada(string t)
+        {
+            switch (t)
+            {
+                case "1":
+                    Console.WriteLine("Opção 1 escolhida: Entrar");
+                    e.Entrar();
+                    break;
+                case "2":
+                    Console.WriteLine("Opção 2 escolhida: Sair");
+                    e.Sair();
+                    break;
+                case "3":
+                    Console.WriteLine("Opção 3 escolhida: Subir");
+                    e.Subir();
+                    break;
+                case "4":
+                    Console.WriteLine("Opção 4 escolhida: Descer");
+                    e.Descer();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
         }
     }
 }
